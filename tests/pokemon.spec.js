81e6ed1267a8a4328b9e69953a91cc336ea3925b
Faz540 <pauljosephfarrell89@gmail.com>
2023-08-16 17:46:46 +0100

    added tests for other endpoint

--- tests/pokemon.spec.js
+++ tests/pokemon.spec.js
@@ -2,18 +2,12 @@ const { makeRequest } = require("../utils/makeRequest");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("/moves", function () {
-	it("should return a list of moves", async function () {
-		const data = await makeRequest("/move?limit=20");
-		expect(data.results).to.be.an("array");
-		expect(data.results).to.have.lengthOf.at.least(1);
-	});
-
-	it("should return a single move", async function () {
-		const data = await makeRequest("/move/1");
+describe("/pokemon", function () {
+	it("should return data for a valid Pokemon", async function () {
+		const data = await makeRequest("/pokemon/pikachu");
 		expect(data).to.be.an("object");
-		expect(data.name).to.equal("pound");
-		expect(data.power).to.equal(40);
-		expect(data.type.name).to.equal("normal");
+		expect(data).to.have.property("id", 25);
+		expect(data).to.have.property("name", "pikachu");
+		expect(data).to.have.property("weight", 60);
 	});
 });
